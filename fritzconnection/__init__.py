75ebbe0972d82f1788003255d375730a8d85fca5
Klaus Bremer bremer <Klaus Bremer bremer@bremer-media.de>
2017-07-12 21:54:10 +0200

    import statements changed

--- fritzconnection/__init__.py
+++ fritzconnection/__init__.py
@@ -1,20 +1,27 @@
 # -*- coding: utf-8 -*-
 
+
 from .fritzconnection import (
     FritzConnection,
     FritzInspection,
     print_servicenames,
     print_api,
 )
+from . fritzcall import FritzCall
+from . fritzcallforwarding import (
+    FritzCallforwarding,
+    print_callforwardings,
+)
 from .fritzhosts import (
     FritzHosts,
     print_hosts,
-)
-from .fritzstatus import (
-    FritzStatus,
-    print_status,
 )
 from .fritzphonebook import (
     FritzPhonebook,
     print_phonebooks,
 )
+from .fritzstatus import (
+    FritzStatus,
+    print_status,
+)
+from .fritzwlan import FritzWLAN
