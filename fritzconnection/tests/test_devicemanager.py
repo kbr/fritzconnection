9494e6b9dc3bbd8b3f954689ca4c03016a66191e
Klaus Bremer <bremer@bremer-media.de>
2019-09-28 15:23:03 +0200

    more DeviceManager tests

--- fritzconnection/tests/test_devicemanager.py
+++ fritzconnection/tests/test_devicemanager.py
@@ -29,10 +29,27 @@ def device_manager():
 
 
 def test_DeviceManager_load_descriptions(device_manager):
-    """test loading od two description files."""
+    """test loading of two description files."""
     assert len(device_manager.descriptions) == 2
 
 
-def test_ServiceManager_modelname(device_manager):
-    """test loading od two description files."""
+def test_DeviceManager_modelname(device_manager):
+    """test loading of two description files."""
     assert device_manager.modelname == 'FRITZ!Box 7590'
+
+
+@pytest.mark.parametrize(
+    "name", ['WANIPConn1', 'DeviceConfig1', 'WLANConfiguration1']
+)
+def test_DeviceManager_get_service(name, device_manager):
+    """
+    test services dict of the DeviceManager. Should find nested services
+    (from sub devices) and from different files.
+    """
+    device_manager.scan()
+    # should not raise a KeyError
+    service = device_manager.services[name]
+    assert service.name == name
+
+
+
