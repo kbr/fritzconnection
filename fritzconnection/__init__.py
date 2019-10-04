da9f5c46d145612be71133961e6ab7ded5681a64
Klaus Bremer <bremer@bremer-media.de>
2019-10-04 17:41:02 +0200

    import shortcuts added

--- fritzconnection/__init__.py
+++ fritzconnection/__init__.py
@@ -1,10 +1,24 @@
-# -*- coding: utf-8 -*-
-
-
-from .core.fritzconnection import (
-    FritzConnection,
-)
+# import shortcuts
 
 __version__ = '1.0_alpha_1'
+
+from .core import (
+    FritzConnection,
+    FritzConnectionException
+    FritzActionError,
+    FritzArgumentError,
+    FritzActionFailedError,
+    FritzArgumentValueError,
+    FritzOutOfMemoryError,
+    FritzSecurityError,
+    FritzArrayIndexError,
+    FritzLookUpError,
+    FritzArgumentStringToShortError,
+    FritzArgumentStringToLongError,
+    FritzArgumentCharacterError,
+    FritzInternalError,
+    ServiceError,
+    ActionError,
+)
 
 package_version = __version__
--- fritzconnection/core/__init__.py
+++ fritzconnection/core/__init__.py
@@ -1,3 +1,20 @@
 # import shortcuts
 
 from .fritzconnection import FritzConnection
+from .exceptions import (
+    FritzConnectionException
+    FritzActionError,
+    FritzArgumentError,
+    FritzActionFailedError,
+    FritzArgumentValueError,
+    FritzOutOfMemoryError,
+    FritzSecurityError,
+    FritzArrayIndexError,
+    FritzLookUpError,
+    FritzArgumentStringToShortError,
+    FritzArgumentStringToLongError,
+    FritzArgumentCharacterError,
+    FritzInternalError,
+    ServiceError,
+    ActionError
+)
