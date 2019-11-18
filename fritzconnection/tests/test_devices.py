abd38a165065950c6aa42b32260769bc97802119
Charlie Clark <charlie.clark@clark-consulting.eu>
2019-11-18 19:54:25 +0100

    Add a simple fixture for getting data files and parametrize tests instead of the fixture. Remove parse_xml because a filename can be passed in but a an ElementTree shouldn't be.

--- fritzconnection/tests/test_devices.py
+++ fritzconnection/tests/test_devices.py
@@ -1,39 +1,31 @@
 from io import StringIO
 from pathlib import Path
+import os
 
 import pytest
 from xml.etree import ElementTree as etree
 
 from ..core.devices import DeviceManager
 
-
-IGDDESC_FILE = Path(__file__).parent/'xml'/'igddesc.xml'
-TR64DESC_FILE = Path(__file__).parent/'xml'/'tr64desc.xml'
-
-
-def parse_xml(source):
-    tree = etree.parse(StringIO(source))
-    root = tree.getroot()
-    return root
-
-
-@pytest.fixture(scope="module")
-def device_manager():
-    manager = DeviceManager()
-    with open(IGDDESC_FILE) as f1, open(TR64DESC_FILE) as f2:
-        manager.add_description(f1)
-        manager.add_description(f2)
-    return manager
-
-
-def test_DeviceManager_load_descriptions(device_manager):
+@pytest.mark.parametrize("filename", [
+    "igddesc.xml",
+    "tr64desc.xml",
+]
+                         )
+def test_DeviceManager_load_descriptions(device_manager, datadir, filename):
     """test loading of two description files."""
-    assert len(device_manager.descriptions) == 2
-
-
-def test_DeviceManager_modelname(device_manager):
-    """test loading of two description files."""
+    os.chdir(datadir)
+    device_manager.add_description(filename)
     assert device_manager.modelname == 'FRITZ!Box 7590'
+
+
+@pytest.fixture()
+def device_manager(datadir):
+    os.chdir(datadir)
+    manager = DeviceManager()
+    manager.add_description('igddesc.xml')
+    manager.add_description('tr64desc.xml')
+    return manager
 
 
 @pytest.mark.parametrize(
@@ -48,6 +40,3 @@ def test_DeviceManager_get_service(name, device_manager):
     # should not raise a KeyError
     service = device_manager.services[name]
     assert service.name == name
-
-
-
