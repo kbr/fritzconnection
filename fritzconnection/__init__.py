993da135e857dc147bb9db578f6a74d3778da8ab
Klaus Bremer <bremer@bremer-media.de>
2019-12-15 14:27:10 +0100

    cleanup module import shortcuts

--- fritzconnection/__init__.py
+++ fritzconnection/__init__.py
@@ -1,25 +1,7 @@
 # import shortcuts
+from .core.fritzconnection import FritzConnection
+from .core import exceptions
+
 
 __version__ = '1.0a1'
-
-from .core import (
-    FritzConnection,
-    FritzConnectionException,
-    FritzServiceError,
-    FritzActionError,
-    FritzArgumentError,
-    FritzActionFailedError,
-    FritzArgumentValueError,
-    FritzOutOfMemoryError,
-    FritzSecurityError,
-    FritzArrayIndexError,
-    FritzLookUpError,
-    FritzArgumentStringToShortError,
-    FritzArgumentStringToLongError,
-    FritzArgumentCharacterError,
-    FritzInternalError,
-    ServiceError,
-    ActionError,
-)
-
 package_version = __version__
