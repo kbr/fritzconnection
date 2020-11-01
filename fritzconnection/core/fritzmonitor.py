456ee912016f47464a6e7aab661033af38281622
Klaus Bremer <bremer@bremer-media.com>
2020-11-01 16:40:41 +0100

    first version of fritzmonitor and corresponding tests.

---
+++ fritzconnection/core/fritzmonitor.py
@@ -0,0 +1,161 @@
+""""
+Module to communicate with the AVM Fritz!Box service providing real time
+phone-call events.
+
+To run this, the CallMonitor service of the box has to be activated.
+This can be done with any registered Phone by typing the following codes:
+activate: #96*5*
+deactivate: #96*4* 
+"""
+# This module is part of the FritzConnection package.
+# https://github.com/kbr/fritzconnection
+# License: MIT (https://opensource.org/licenses/MIT)
+# Author: Klaus Bremer
+
+
+import queue
+import socket
+import threading
+
+
+FRITZ_IP_ADDRESS = "169.254.1.1"
+FRITZ_MONITOR_PORT = 1012
+FRITZ_MONITOR_QUEUE_SIZE = 256
+FRITZ_MONITOR_CHUNK_SIZE = 1024 * 4
+FRITZ_MONITOR_SOCKET_TIMEOUT = 10
+
+
+class EventReporter:
+    """
+    Takes a Queue and implements a buffer for line-separated data.
+    If at least one line is in the buffer, the line gets put into the 
+    Queue for further processing elsewhere (by the Queue-reader).
+    """
+
+    def __init__(self, monitor_queue, block_on_filled_queue=False):
+        """
+        Takes the monitor queue (of type queue.Queue) and a boolean
+        'block_on_filled_queue'. If 'block_on_filled_queue' is True the thread
+        will wait until a free slot is available blocking the thread reading
+        call_monitor events from the socket connected to the Fritz!Box.
+        """
+        self.buffer = ""
+        self.monitor_queue = monitor_queue
+        self.block_on_filled_queue = block_on_filled_queue
+
+    def add(self, data):
+        """
+        Adds the given 'data' to the buffer. If the buffer holds at least one
+        line (separated by '\n'), the lines are put into the 'monitor_queue' to
+        be processed from a reader elsewhere.
+        """
+        self.buffer += data
+        *parts, self.buffer = self.buffer.split("\n")
+        for part in parts:
+            try:
+                self.monitor_queue.put(part, block=self.block_on_filled_queue)
+            except queue.Full:
+                # ignore
+                pass
+
+
+class FritzMonitor:
+    """
+    Monitor Fritz!Box events about started, finished, incoming and outgoing
+    calls.
+    """
+
+    def __init__(
+        self,
+        address=FRITZ_IP_ADDRESS,
+        port=FRITZ_MONITOR_PORT,
+        timeout=FRITZ_MONITOR_SOCKET_TIMEOUT,
+        logger=None,
+        encoding="utf-8",
+    ):
+        self.address = address
+        self.port = port
+        self.timeout = timeout
+        self.logger = logger
+        self.stop_flag = threading.Event()
+        self.monitor_thread = None
+        self.encoding = encoding
+
+    def start(
+        self,
+        queue_size=FRITZ_MONITOR_QUEUE_SIZE,
+        block_on_filled_queue=False,
+        sock=None,
+    ):
+        """
+        Start the monitor thread and return a Queue instance with the given size
+        to report the fritzcall events.
+        Raises an OSError if the socket can not get connected in a given timeout.
+        Raises a RuntimeError if start() get called a second time without calling
+        stop() first.
+        """
+        if self.monitor_thread:
+            # It's an error to create a second thread for monitoring
+            raise RuntimeError("A FritzMonitor thread is already running")
+        # get socket or raise OSError in main thread:
+        sock = self._get_connected_socket(sock=sock)
+        monitor_queue = queue.Queue(maxsize=queue_size)
+        kwargs = {
+            "monitor_queue": monitor_queue,
+            "sock": sock,
+            "block_on_filled_queue": block_on_filled_queue,
+        }
+        # clear event object in case the instance gets 'reused':
+        # self.stop_flag.clear()
+        self.monitor_thread = threading.Thread(target=self._monitor, kwargs=kwargs)
+        self.monitor_thread.start()
+        return monitor_queue
+
+    def stop(self):
+        """
+        Stop the current running _monitor-thread.
+        """
+        if self.monitor_thread:
+            if self.monitor_thread.is_alive():
+                self.stop_flag.set()  # tell thread to terminate
+                self.monitor_thread.join()  # wait to terminate without timeout
+            self.monitor_thread = None
+
+    def _get_connected_socket(self, sock=None):
+        if sock is None:
+            sock = socket.socket()
+            sock.setsockopt(socket.SOL_SOCKET, socket.SO_KEEPALIVE, 1)
+            # socket actions can fail for different reasons,
+            # without a timeout they can hang a long time.
+            sock.settimeout(self.timeout)
+        try:
+            sock.connect((self.address, self.port))
+        except socket.timeout:
+            # will fail if the address:port is not available (not present or in use)
+            # socket.timeout is subclass of OSError, so it is save to raise an OSError
+            msg = f"Unable to connect to '{self.address}:{self.port}'"
+            raise OSError(msg)
+        return sock
+
+    def _monitor(self, monitor_queue, sock, block_on_filled_queue):
+        event_reporter = EventReporter(
+            monitor_queue=monitor_queue, block_on_filled_queue=block_on_filled_queue
+        )
+        while not self.stop_flag.is_set():
+            try:
+                raw_data = sock.recv(FRITZ_MONITOR_CHUNK_SIZE)
+            except socket.timeout:
+                # without a timeout an open socket will never return from a
+                # connection closed by a router (may be of limited resources).
+                # So just try again after timeout.
+                continue
+            if not raw_data:
+                # empty response indicates a lost connection.
+                # try to reconnect.
+                ...
+            else:
+                # sock.recv returns a bytearray to decode:
+                response = raw_data.decode(self.encoding)
+                event_reporter.add(response)
+        # clean up on terminating thread:
+        sock.close()
---
+++ fritzconnection/tests/test_fritzmonitor.py
@@ -0,0 +1,129 @@
+import queue
+import socket
+import threading
+import time
+
+import pytest
+import requests
+
+from ..core.fritzmonitor import FritzMonitor, EventReporter
+
+
+class MockSocket:
+    def __init__(self, mock_data=None, timeout=None):
+        self.mock_data = mock_data
+        self.timeout = timeout
+
+    def connect(self, *args):
+        pass
+
+    def close(self):
+        pass
+
+    def recv(self, chunk_size=None):
+        if not self.mock_data:
+            if self.timeout:
+                time.sleep(self.timeout)
+            raise socket.timeout("mock timeout")
+        if chunk_size is None:
+            chunk = self.mock_data.encode("utf-8")
+        else:
+            chunk = self.mock_data[:chunk_size]
+            self.mock_data = self.mock_data[chunk_size:]
+        return chunk.encode("utf-8")
+
+
+def test_init_fritzmonitor():
+    fm = FritzMonitor()
+    assert fm.monitor_thread == None
+    assert fm.stop_flag.is_set() == False
+
+
+@pytest.mark.parametrize(
+    "data, expected",
+    [
+        ("some data", None),
+        ("some_data\nmore_data", "some_data"),
+        ("some\nmore\ndata", "some more"),
+        ("2019;10\n11:30\ncall\n123\nmore", "2019;10 11:30 call 123"),
+    ],
+)
+def test_event_reporter(data, expected):
+    q = queue.Queue()
+    ep = EventReporter(q)
+    ep.add(data)
+    if expected is None:
+        # no data should be in the queue
+        assert pytest.raises(queue.Empty, q.get_nowait)
+    else:
+        for event in expected.split():
+            item = q.get_nowait()
+            assert item == event
+
+
+def test_start_stop():
+    mock_socket = MockSocket(timeout=0.01)
+    fm = FritzMonitor()
+    event_queue = fm.start(sock=mock_socket)
+    assert isinstance(event_queue, queue.Queue)
+    assert isinstance(fm.monitor_thread, threading.Thread)
+    assert fm.monitor_thread.is_alive() is True
+    thread = fm.monitor_thread
+    fm.stop()
+    assert thread.is_alive() is False
+    assert fm.monitor_thread is None
+
+
+@pytest.mark.parametrize(
+    "mock_data, chunk_size, expected_events",
+    [
+        ("simple string of words\n", None, "simple string of words"),
+        ("event;call\nnumber\n12345\n", None, "event;call|number|12345"),
+        (
+            "CALL;20201031;12;34\nINC;20-10-31;09;76\n",
+            None,
+            "CALL;20201031;12;34|INC;20-10-31;09;76",
+        ),
+        ("1\n2\n3\n4\n5\n6\n7\n8\n9\n10\n11\n", None, "1|2|3|4|5|6|7|8|9|10|11"),
+        ("simple string of words\n", 2048, "simple string of words"),
+        ("event;call\nnumber\n12345\n", 2048, "event;call|number|12345"),
+        (
+            "CALL;20201031;12;34\nINC;20-10-31;09;76\n",
+            2048,
+            "CALL;20201031;12;34|INC;20-10-31;09;76",
+        ),
+        ("1\n2\n3\n4\n5\n6\n7\n8\n9\n10\n11\n", 2048, "1|2|3|4|5|6|7|8|9|10|11"),
+        ("simple string of words\n", 20, "simple string of words"),
+        ("event;call\nnumber\n12345\n", 20, "event;call|number|12345"),
+        (
+            "CALL;20201031;12;34\nINC;20-10-31;09;76\n",
+            20,
+            "CALL;20201031;12;34|INC;20-10-31;09;76",
+        ),
+        ("1\n2\n3\n4\n5\n6\n7\n8\n9\n10\n11\n", 20, "1|2|3|4|5|6|7|8|9|10|11"),
+        ("simple string of words\n", 2, "simple string of words"),
+        ("event;call\nnumber\n12345\n", 2, "event;call|number|12345"),
+        (
+            "CALL;20201031;12;34\nINC;20-10-31;09;76\n",
+            2,
+            "CALL;20201031;12;34|INC;20-10-31;09;76",
+        ),
+        ("1\n2\n3\n4\n5\n6\n7\n8\n9\n10\n11\n", 2, "1|2|3|4|5|6|7|8|9|10|11"),
+    ],
+)
+def test_get_events(mock_data, chunk_size, expected_events):
+    mock_socket = MockSocket(mock_data=mock_data, timeout=0.01)
+    fm = FritzMonitor()
+    event_queue = fm.start(sock=mock_socket)
+    thread = fm.monitor_thread
+    received_event_num = 0
+    for n, expected_event in enumerate(expected_events.split("|"), start=1):
+        try:
+            assert event_queue.get(timeout=0.1) == expected_event
+            received_event_num += 1
+        except queue.Empty:
+            pass
+    # check if alle expected events have been received
+    assert received_event_num == n
+    fm.stop()
+    assert thread.is_alive() is False
