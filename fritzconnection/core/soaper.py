45c274fde26c5bcbec358465fdfbc3d2c565e895
kbr <bremer@bremer-media.com>
2021-07-29 14:17:56 +0200

    logger calls included in utils and soaper.

--- fritzconnection/core/soaper.py
+++ fritzconnection/core/soaper.py
@@ -19,6 +19,7 @@ from .exceptions import (
     FritzConnectionException,
     FRITZ_ERRORS,
 )
+from .logger import fritzlogger
 from .utils import localname
 
 
@@ -234,6 +235,8 @@ class Soaper:
         """
 
         def handle_response(response):
+            fritzlogger.log(f"response status: {response.status_code}", "debug")
+            fritzlogger.log(response.text, "debug")
             if response.status_code != 200:
                 raise_fritzconnection_error(response)
             return self.parse_response(response, service, action_name)
@@ -247,6 +250,8 @@ class Soaper:
         body = self.get_body(service, action_name, arguments)
         envelope = self.envelope.format(body=body).encode("utf-8")
         url = f"{self.address}:{self.port}{service.controlURL}"
+        fritzlogger.log(f"\n{url}", "debug")
+        fritzlogger.log(envelope, "debug")
         if self.session:
             with self.session.post(
                 url, data=envelope, headers=headers, timeout=self.timeout
--- fritzconnection/core/utils.py
+++ fritzconnection/core/utils.py
@@ -7,6 +7,7 @@ import requests
 
 from xml.etree import ElementTree as etree
 from .exceptions import FritzConnectionException
+from .logger import fritzlogger
 
 
 NS_REGEX = re.compile("({(?P<namespace>.*)})?(?P<localname>.*)")
@@ -28,12 +29,14 @@ def get_content_from(url, timeout=None, session=None):
     verification will fail.
     """
     def handle_response(response):
+        fritzlogger.log(response.text, "debug")
         ct = response.headers.get("Content-type")
         if ct == "text/html":
             message = f"Unable to retrieve resource '{url}' from the device."
             raise FritzConnectionException(message)
         return response.text
 
+    fritzlogger.log(f"\n{url}", "debug")
     if session:
         with session.get(url, timeout=timeout) as response:
             return handle_response(response)
