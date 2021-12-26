5db2dbfb792d57b81e4987ce543e5641577332c3
Klaus Bremer <bremer@bremer-media.com>
2021-12-26 15:06:22 +0100

    improve logger module and refactoring of the according tests

--- fritzconnection/core/logger.py
+++ fritzconnection/core/logger.py
@@ -1,10 +1,10 @@
 """
 Logging module for the fritzconnection library. By default
-fritzconnection will emit all messages with a log-level of INFO or
+fritzconnection will emit all messages with a log-level of INFO and
 higher (WARNING, ERROR, CRITICAL).
 
-The fritzconnection logger is defined as `fritzlogger` and is an
-instance of the `logging.Logger` class from the stdlib and can get
+The fritzconnection logger, defined as `fritzlogger`, is an
+instance of the `logging.Logger` class from the `stdlib` and can get
 accessed by:
 
 >>> from fritzconnection.core.logger import fritzlogger
@@ -12,12 +12,31 @@ accessed by:
 This module provides the additional functions
 `activate_local_debug_mode` and `reset` (see below) to activate a debug
 mode, suppressing handler-propagation, and to reset the logger to the
-initial state.
+initial state. To activate debug logging for fritzconnection call:
 
-In debug mode fritzconnection will log the data transfered between the
-library and the router. That can be a lot of data, especially on
-instanciating FritzConnection.
+>>> from fritzconnection.core.logger import activate_local_debug_mode
+>>> activate_local_debug_mode(handler=logging.StreamHandler())
+
+In this debug mode fritzconnection will additionally log the data
+transfered between the library and the router and suppress
+logging-propagation to the parent-handlers. Here the `StreamHandler` will
+send all data to `stderr`. Because this can be a lot of data a `FileHandler`
+may be a better choice:
+
+>>> activate_local_debug_mode(handler=logging.FileHandler(<the_file>))
+
+To deactivate this mode call `reset()`:
+
+>>> from fritzconnection.core.logger import reset
+>>> reset()
+
+Beside this you can do everything with `fritzlogger` as with the
+`logging.Logger`, because `fritzlogger` is an instance of the latter.
 """
+# This module is part of the FritzConnection package.
+# https://github.com/kbr/fritzconnection
+# License: MIT (https://opensource.org/licenses/MIT)
+
 
 import logging
 
@@ -28,16 +47,17 @@ fritzlogger.setLevel(logging.INFO)
 
 def activate_local_debug_mode(handler=None, propagate=False):
     """
-    Activates all logging messages on debug level but don't propagate to
-    parent-handlers. However, if no handler is set at all, messages will
-    propagate to the lastResort which by default is a StreamHandler with
-    level.NOTSET, therefore emitting everything to stderr.
-    It can be useful to provide a file-handler because running
-    fritzlogger with DEBUG level will produce a lot of output.
+    Activates all logging messages on debug level and don't propagate to
+    parent-handlers. If no handler is given the NullHandler will get
+    set, avoiding a call of the lastResort-handler.
+    If propagate is True, all debug informations will also get send to
+    the parent-handlers. Keep in mind, that this can be a lot of data if
+    the parent-handlers are enabled for debug-level records.
     """
+    if handler is None:
+        handler = logging.NullHandler()
+    fritzlogger.addHandler(handler)
     fritzlogger.propagate = propagate
-    if handler:
-        fritzlogger.addHandler(handler)
     fritzlogger.setLevel(logging.DEBUG)
 
 
