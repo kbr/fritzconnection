b35f6adc4c836235f6fec0174b963f0274097006
chemelli74 <simone.chemelli@gmail.com>
2022-02-12 22:33:35 +0000

    Mark as property

--- fritzconnection/core/fritzconnection.py
+++ fritzconnection/core/fritzconnection.py
@@ -185,6 +185,15 @@ class FritzConnection:
         )
 
     @property
+    def device_is_router(self):
+        """
+        Return True is the device is a router.
+        """
+        if connection_type := self.device_connection_type():
+            return self.call_action(connection_type, "GetInfo").get("NewEnable")
+        return False
+
+    @property
     def services(self):
         """
         Dictionary of service instances. Keys are the service names.
@@ -306,10 +315,3 @@ class FritzConnection:
         """
         self.call_action("DeviceConfig1", "Reboot")
 
-    def device_is_router(self):
-        """
-        Return True is the device is a router.
-        """
-        if connection_type := self.device_connection_type():
-            return self.call_action(connection_type, "GetInfo").get("NewEnable")
-        return False
