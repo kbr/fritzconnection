27433100a07e4a99c38d9df719aa5f0d8c3270ef
chemelli74 <simone.chemelli@gmail.com>
2022-07-23 15:17:16 +0000

    Apply review comment

--- fritzconnection/lib/fritzstatus.py
+++ fritzconnection/lib/fritzstatus.py
@@ -304,11 +304,48 @@ class FritzStatus(AbstractLibraryBase):
         return status["NewEnable"]
 
     @property
-    def wan_enabled(self):
+    def device_has_mesh_support(self):
         """
-        Return True is the device is using a WAN connnection.
+        True if the device supports mesh, otherwise False.
         """
-        if not self.connection_type:
+        # check for the corresponding action
+        # whether mesh is supported
+        try:
+            return (
+                "X_AVM-DE_GetMeshListPath"
+                in self.fc.services["Hosts1"].actions
+            )
+        except KeyError:
+            # can happen if "Hosts1" is not known
             return False
 
-        return self.fc.call_action(self.connection_type, "GetInfo").get("NewEnable")
+    def get_default_connection_service(self):
+        """
+        Returns a namedtuple of type DefaultConnectionService:
+        `prefix` -> str
+        `device_connection` -> str (like "WANPPPConnection")
+        `postfix` -> str
+        """
+        result = self.fc.call_action(
+                "Layer3Forwarding1", "GetDefaultConnectionService"
+        )
+        prefix, connection_service, postfix = \
+            result["NewDefaultConnectionService"].split('.', 2)
+        return DefaultConnectionService(
+            prefix, connection_service, postfix
+        )
+
+    @property
+    def update_available(self):
+        """
+        The new version number (as a string) if an update is available or an
+        empty string if no update is avilable.
+        """
+        return self.fc.call_action("UserInterface1", "GetInfo")["NewX_AVM-DE_Version"]
+
+    @property
+    def has_wan_enabled(self):
+        """
+        True if wan is enabled otherwise False.
+        """
+        return self.fc.call_action(self.get_default_connection_service().connection_service, "GetInfo")["NewEnable"]
