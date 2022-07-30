457eaefa5a5c38507c4510b0c20fe88d801b8ee8
Klaus Bremer <bremer@bremer-media.com>
2022-07-30 15:27:27 +0200

    typo removed

--- fritzconnection/lib/fritzhosts.py
+++ fritzconnection/lib/fritzhosts.py
@@ -205,7 +205,7 @@ class FritzHosts(AbstractLibraryBase):
         returned data in a list of dictionaries describing the known
         hosts.
 
-        .. versionadded :: development
+        .. versionadded:: development
         """
         result = self._action("X_AVM-DE_GetHostListPath")
         url = result["NewX_AVM-DE_HostListPath"]
