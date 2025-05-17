7476f9a05526351a00d6b398babc106aa038ae2d
Klaus Bremer <bremer@bremer-media.com>
2025-05-17 13:04:34 +0200

    fixed reference because deletion of deprecated code

--- fritzconnection/lib/fritzstatus.py
+++ fritzconnection/lib/fritzstatus.py
@@ -160,7 +160,7 @@ class FritzStatus(AbstractLibraryBase):
     @property
     def str_uptime(self) -> str:
         """Connection uptime in human-readable format."""
-        mins, secs = divmod(self.uptime, 60)
+        mins, secs = divmod(self.connection_uptime, 60)
         hours, mins = divmod(mins, 60)
         return "%02d:%02d:%02d" % (hours, mins, secs)
 
